8100ff51ddc5695c9219c076d594ce0f3c2a0aa5
calebwelch <caleb.welch11@gmail.com>
2020-04-17 21:47:57 -0400

    Added randomizer logic. Added element tags in js.

--- password.js
+++ password.js
@@ -1,16 +1,18 @@
-lowerEl = document.querySelector("#lowercaseText");
 
-lowerEl.setAttribute("style", "color:red");
 
 // .checked method to see if a checkbox is checked or not
 // if (document.querySelector('#lowercase').checked) {
 //     alert("The box is checked!");
 // }
-//checkboxes
-var lowerCheck = document.querySelector("#lowercase").checked
-var upperCheck = document.querySelector("#uppercase").checked
-var numCheck = document.querySelector("#numbers").checked
-var symbolCheck = document.querySelector("#symbols").checked
+//                                                      Elements
+var lowerEl = document.querySelector("#lowercase")
+var upperEl = document.querySelector("#uppercase")
+var numEl = document.querySelector("#numbers")
+var symbolEl = document.querySelector("#symbols")
+
+var passwordLength = document.querySelector("#passLength")
+var genButton = document.querySelector("#generatorButton")
+
 //password Output and a variable to store a generated password in
 var passwordOut = document.querySelector("#passwordOutput")
 var newPassword = " ";
@@ -28,7 +30,6 @@ function randomNum() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-console.log(randomNum());
 
 // now to do the same with upper and lowercase letters
 // upper chars appear to be 65 - 90
@@ -36,12 +37,56 @@ function randomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
-console.log(randomUpper())
 // lowercase is as easy as taking the randomUpper function and adding .toLowerCase() at the end
 function randomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65).toLowerCase()
 }
 
-console.log(randomLower())
+function randomSymbol() {
+    // symbol list 0-14 need to pick a random symbol from this list.
+    return symbolList[Math.floor(Math.random() * 14)];
+}
+// now lets make a variable for length and the generator button
 
-// now that I have the random functions generated I have to attach them to a boolean that checks if a checkbox is checked or not
+// now that I have the random functions generated I have to attach event listers to see what the user has input on the form.
+// maybe I can just add something on the generator button that looks at each part of the form rather than updating it whenever a checkbox is clicked.
+genButton.addEventListener("click", function () {
+    //get values for elements above.
+    // unary operator '+' will make it an int
+    var length = +passwordLength.value;
+    var lowerCheck = lowerEl.checked;
+    var upperCheck = upperEl.checked;
+    var symbolCheck = symbolEl.checked;
+    var numCheck = numEl.checked;
+})
+
+
+//I need to make a function that creates a random password based on the input from the user.
+randomArr = [randomNum(), randomLower(), randomUpper(), randomSymbol()]
+// randomly selects one of the randomizers.
+function randomRandomizer() {
+    return randomArr[Math.floor(Math.random() * randomArr.length)]
+}
+
+// function passwordGenerator() {
+//     for (i = 0; i < passwordLength; i++) {
+//         if (lowerCheck === false) {
+//             delete randomArr[1];
+//         }
+//         if (upperCheck === false) {
+//             delete randomArr[2];
+//         }
+//         if (numCheck === false) {
+//             delete randomArr[0];
+//         }
+//         if (symbolCheck === false) {
+//             delete randomArr[3];
+//         }
+//         else {
+//             newPassword += randomRandomizer();
+//             giveNewPass();
+//         }
+//     }
+// }
+
+
